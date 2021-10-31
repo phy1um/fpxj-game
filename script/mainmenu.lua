0af82ecae496cb7a74251a3b8f684db548933cb0
Tom Marks <tom@tommarks.xyz>
2021-11-01 04:04:30 +1100

    menu configurable

--- script/mainmenu.lua
+++ script/mainmenu.lua
@@ -4,6 +4,10 @@ local T = require("text")
 local mm = {
   cursorPos = 1,
   actions = {},
+  itemSep = 30,
+  margin = {x=100, y=60},
+  baseCol = {r=0xff,g=0xff,b=0xff},
+  highlight = {r=0xff,g=0,b=0},
 }
 
 function mm:addEntry(name, action)
@@ -17,11 +21,11 @@ end
 function mm:draw()
   for i, v in ipairs(self.actions) do
     if i == self.cursorPos then
-      D2D:setColour(0xff, 0, 0, 0x80)
+      D2D:setColour(self.highlight.r, self.highlight.g, self.highlight.b, 0x80)
     else
-      D2D:setColour(0xff, 0xff, 0xff, 0x80)
+      D2D:setColour(self.baseCol.r, self.baseCol.g, self.baseCol.b, 0x80)
     end
-    T.printLines(100, 60 + (i-1)*30, v.name) 
+    T.printLines(self.margin.x, self.margin.y + (i-1)*self.itemSep, v.name) 
   end
 end
 
